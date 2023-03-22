7a327ad24862e9cce1dc88bb856e0d2aaa24fc74
SahajPurohit <72097782+SahajPurohit@users.noreply.github.com>
2023-03-22 19:18:00 -0400

    Add overlay for popup

--- js/term.js
+++ js/term.js
@@ -1,24 +1,28 @@
 // Logic to show form
 document.querySelector("#add-app").addEventListener("click", function() {
     document.querySelector(".popup-form").classList.add("active");
+    document.querySelector("#overlay").classList.add("active");
 });
 
 // Close btn logic
 document.querySelector(".popup-form .close-btn").addEventListener("click", function() {
     document.querySelector(".popup-form").classList.remove("active");
+    document.querySelector("#overlay").classList.remove("active");
 });
 
 // Should also close after clicking enter
 document.querySelector(".popup-form .enter-btn").addEventListener("click", function() {
     document.querySelector(".popup-form").classList.remove("active");
+    document.querySelector("#overlay").classList.remove("active");
 });
 
 
 var tableContent = [];
 var ctr = 1;
+// table = document.getElementById("table")
 
 function addApps() {
-    table = document.getElementById("table")
+    // table = document.getElementById("table")
     var dateAndTime = Date();
     date = dateAndTime.substring(0, 15);
     time = dateAndTime.substring(17, 24);
@@ -28,13 +32,24 @@ function addApps() {
     renderTable();
 }
 
+function deleteLatestEntry() {
+    console.log("Ello")
+    if (tableContent.length == 0) return;
+    tableContent.pop();
+    // table = document.getElementById("table")
+    renderTable();
+}
+
 function renderTable() {
+    // table = document.getElementById("table")
+    // Add the numbering and the status
     for (let i = ctr - 1; i < tableContent.length; i++) {
         tableContent[i].unshift(ctr.toString());
         ctr++;
         tableContent[i].push("Active");
     }
 
+    // Populate the table
     for (let i = 1; i <= tableContent.length; i++) { // rows
         for (let j = 0; j < tableContent[i-1].length; j++) { // cells
             table.rows[i].cells[j].innerHTML = tableContent[i-1][j];
