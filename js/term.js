5e7a2ee4221096f2323c3f3c10d798fe4ef6f692
SahajPurohit <72097782+SahajPurohit@users.noreply.github.com>
2023-03-22 03:24:57 -0400

    Add logic to close popup after clicking enter
    Note: At this point, there is a limitation on the maximum number of rows the table can have because the HTML table only contains a certain number of rows. Ability to dynamically resize the HTML table to be added later on.

--- js/term.js
+++ js/term.js
@@ -8,30 +8,37 @@ document.querySelector(".popup-form .close-btn").addEventListener("click", funct
     document.querySelector(".popup-form").classList.remove("active");
 });
 
-table = document.getElementById("table")
-var dateAndTime = Date();
-date = dateAndTime.substring(0, 15);
-time = dateAndTime.substring(17, 24);
-// var tableHeaders = ["#", "Company", "Position", "Date Applied", "Time Applied", "Status"]
-var tableContent = [["#", "Company", "Position", "Date Applied", "Time Applied", "Status"],
-                ["Apple", "Software Engineer", date, time],
-                ["CashApp", "Software Engineer", date, time],
-                ["Adobe", "Software Engineer", date, time],
-                ["Amazon", "SWE", date, time]];
-
-var ctr = 1;
-
-for (let i = 1; i < tableContent.length; i++) {
-    tableContent[i].unshift(ctr.toString());
-    ctr++;
-    tableContent[i].push("Active")
-}
-
-console.log(tableContent)
+// Should also close after clicking enter
+document.querySelector(".popup-form .enter-btn").addEventListener("click", function() {
+    document.querySelector(".popup-form").classList.remove("active");
+});
 
 
-for (let i = 0; i < table.rows.length; i++) { // rows
-    for (let j = 0; j < table.rows[i].cells.length; j++) { // cells
-        table.rows[i].cells[j].innerHTML = tableContent[i][j];
+function addApps() {
+    table = document.getElementById("table")
+    var dateAndTime = Date();
+    date = dateAndTime.substring(0, 15);
+    time = dateAndTime.substring(17, 24);
+    // var tableHeaders = ["#", "Company", "Position", "Date Applied", "Time Applied", "Status"]
+    var tableContent = [/*["#", "Company", "Position", "Date Applied", "Time Applied", "Status"],*/
+                    ["Apple", "Software Engineer", date, time],
+                    ["CashApp", "Software Engineer", date, time],
+                    ["Adobe", "Software Engineer", date, time],
+                    ["Amazon", "SWE", date, time],
+                    ["Dapple", "SWE", date, time],
+                    ["Amazon", "SWE", date, time],
+                    ["Dapple", "SWE", date, time]];
+
+    var ctr = 1;
+    for (let i = 0; i < tableContent.length; i++) {
+        tableContent[i].unshift(ctr.toString());
+        ctr++;
+        tableContent[i].push("Active")
     }
-}
+
+    for (let i = 1; i <= tableContent.length; i++) { // rows
+        for (let j = 0; j < tableContent[i-1].length; j++) { // cells
+            table.rows[i].cells[j].innerHTML = tableContent[i-1][j];
+        }
+    }
+};
