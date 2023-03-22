091a9bd270d61164a246b786d80693c5f69524b6
SahajPurohit <72097782+SahajPurohit@users.noreply.github.com>
2023-03-22 01:09:51 -0400

    Create logic for popup form to add applications

--- js/term.js
+++ js/term.js
@@ -3,3 +3,14 @@ function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
+
+
+// Logic to show form
+document.querySelector("#add-app").addEventListener("click", function() {
+    document.querySelector(".popup-form").classList.add("active");
+});
+
+// Close btn logic
+document.querySelector(".popup-form .close-btn").addEventListener("click", function() {
+    document.querySelector(".popup-form").classList.remove("active");
+});
